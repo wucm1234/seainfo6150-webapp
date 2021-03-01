9e5a6ebee6d18fe728eb4fa3f29634e270ceed63
wucm1234 <awucm@hotmail.com>
2021-02-28 20:55:53 -0800

    artical fetch

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import HTMLText from "../HTMLText/HTMLText";
 
 const DynamicArticle = (props) => {
+
+  const emailAddr = props.article.authorEmail;
+  const articleDate = props.article.timeStamp.substring(0, 10);
   return (
     <article>
-      {/* add extra html with javascript attributes here */}
-      <HTMLText text={props.article.text} />
+      <header>
+        <title></title>
+        <h1>{props.article.title}</h1>
+        <address>
+          by {props.article.author} (
+          <a href={emailAddr}>{props.article.authorEmail}</a>)
+          <br />
+        </address>
+          <time dateTime={articleDate}>{props.article.displayDate}</time>
+      </header>
+      <HTMLText text={props.article.text} ></HTMLText>
     </article>
   );
 };
